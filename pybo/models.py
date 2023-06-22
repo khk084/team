f55429265e7796cb2b162b4ff9da274cd327e38b
Ha <rla1gkrud1015@gmail.com>
2023-06-22 15:47:04 +0900

    06.22

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Suggestion(models.Model):
+    fName=models.CharField(max_length=200)
+    fMenu=models.CharField(max_length=200)
+    select_date = models.DateTimeField()
+
+class Food(models.Model):
+    name = models.CharField(max_length=100)
+    menu = models.CharField(max_length=100)
+    price = models.CharField(max_length=100)
+
+def __str__(self):
+    return self.name
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Food
+import random
 # Create your views here.
 
 def index(request) :
     return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+def recommend_food(request):
+    #모든 음식 조회
+    all_food = Food.objects.all()
+
+    # 음식이 존재하지 않을 경우에 대한 예외 처리
+
+    if not all_food:
+        return render(request, 'pybo/no_food.html')
+
+    # 랜덤한 음식을 선택
+    random_food = random.choice(all_food)
+
+    context = {
+        'food': random_food
+    }
+
+    return render(request, 'pybo/recommend_food.html', context)
