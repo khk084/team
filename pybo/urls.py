9eb5c79eeae2cef970208b89375e5b24890b5af4
Ha <rla1gkrud1015@gmail.com>
2023-06-23 09:09:39 +0900

    06.23

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.recommend_food),
+    path('', views.index),
+    # path('', views.recommend_food, name='recommend_food'),
 
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,11 +1,38 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .form import UserForm
 from django.http import HttpResponse
 from .models import Food
 import random
 # Create your views here.
 
-def index(request) :
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+
+def index(request):
+    return render(request, 'base.html')
+
+def login_page(request):
+    """
+    로그인 페이지
+    """
+    return render(request, 'login.html')
+
+# 회원가입
+def signup(request):
+    """
+    회원가입 페이지
+    """
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'signup.html', {'form': form})
 
 def recommend_food(request):
     #모든 음식 조회
@@ -24,3 +51,5 @@ def recommend_food(request):
     }
 
     return render(request, 'pybo/recommend_food.html', context)
+
+
