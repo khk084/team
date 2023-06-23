97f5e390af019e2a05813ba398076570773c7c7c
박응용 <pahkey@gmail.com>
2023-06-23 16:17:58 +0900

    오류 수정

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
 from . import views
-
-
-
 urlpatterns = [
     path('', views.index),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_page, name='login'),
-    path('index/', views.index, name='index'),
 
     # path('', views.recommend_food, name='recommend_food'), ddd
 ]
