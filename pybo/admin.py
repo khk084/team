72a2e48cc7e2f367bb4ce695221cfbb09ec90554
ycs <imbu26@gmail.com>
2023-06-23 17:51:24 +0900

    0623_1551

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Records
 # Register your models here.
 
 admin.site.register(Food)
+admin.site.register(Records)
