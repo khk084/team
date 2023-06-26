41337c981bfa97e12057f239271a9664b199708a
ycs <imbu26@gmail.com>
2023-06-26 15:27:50 +0900

    path 수정

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('signup/', views.signup, name='signup'),
+    # path('signup/', views.signup, name='signup'),
     path('login/', views.login_page, name='login'),
     path('records/', views.records, name="records"),
     path('<int:records_id>/', views.records_detail, name="re_detail"),
     path('records/<int:records_id>/submit-rating/', views.submit_rating, name='submit_rating'),
+    path('records/records_create/', views.records_create, name="records_create")
     # path('', views.recommend_food, name='recommend_food'),
 ]
