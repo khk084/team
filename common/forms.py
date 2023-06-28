429a8aee080251a4cf047681a8424bf6027213fe
ycs <imbu26@gmail.com>
2023-06-28 18:47:31 +0900

    회원정보 추가

--- common/forms.py
+++ common/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 from pybo.models import Records
 from common.models import CustomUser
@@ -22,3 +22,14 @@ class RecordsForm(forms.ModelForm):
             'subject': forms.TextInput(attrs={'class':'form-control'}),
             'content': forms.Textarea(attrs={'class':'form-control', 'rows': 10}),
         }
+
+class UserProfileForm(UserChangeForm):
+    first_name = forms.CharField(label="성")
+    last_name = forms.CharField(label="이름")
+    email = forms.EmailField(label="이메일")
+    phone = forms.CharField(label="전화번호")
+    nickname = forms.CharField(label="닉네임")
+    address = forms.CharField(label="주소")
+    class Meta:
+        model = CustomUser
+        fields = ['first_name', 'last_name', 'nickname', 'username', 'phone', 'email', 'address']
